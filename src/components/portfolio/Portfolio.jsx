4b2c2a56165de082d39d3b30319e73fe661e6efc
chriswo92 <chrisworkingonline@gmail.com>
2023-03-01 21:56:57 -0500

    portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,77 @@
 import React from 'react';
 import './portfolio.css';
+import IMG from '../../assets/portfolio.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG,
+    title: 'This is a Portfolio Item Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 2,
+    image: IMG,
+    title: 'This is a Portfolio Item Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 3,
+    image: IMG,
+    title: 'This is a Portfolio Item Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 4,
+    image: IMG,
+    title: 'This is a Portfolio Item Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 5,
+    image: IMG,
+    title: 'This is a Portfolio Item Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  },
+  {
+    id: 6,
+    image: IMG,
+    title: 'This is a Portfolio Item Title',
+    github: 'https://github.com',
+    demo: 'https://dribble.com'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item__image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item__cta">
+                  <a href={github} className='btn' target="__blank">Github</a>
+                  <a href={demo} className='btn btn-primary' target="__blank">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
