d939438d64049d48d08c0af3c04549d0d699a9a4
chriswo92 <chrisworkingonline@gmail.com>
2023-03-04 15:30:49 -0500

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './footer.css';
+import {FaFacebookF} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>chrisWO92</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="experience">Experience</a></li>
+        <li><a href="services">Service</a></li>
+        <li><a href="portfolio">Portfolio</a></li>
+        <li><a href="Testimonials">Testimonials</a></li>
+        <li><a href="contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://facebook.com" target='_blank' rel="noreferrer"><FaFacebookF /></a>
+        <a href="https://instagram.com" target='_blank' rel="noreferrer"><FaInstagram /></a>
+        <a href="https://twitter.com"  target='_blank' rel="noreferrer"><FaTwitter /></a>
+      </div>
+
+    </footer>
   )
 }
 
