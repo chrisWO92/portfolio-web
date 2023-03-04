2dd311f2fc9c8701891ca5a7420e2a0f5dbadbf4
chriswo92 <chrisworkingonline@gmail.com>
2023-03-04 16:52:47 -0500

    finised project

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,18 +11,22 @@ const Footer = () => {
       <a href="#" className='footer__logo'>chrisWO92</a>
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="experience">Experience</a></li>
-        <li><a href="services">Service</a></li>
-        <li><a href="portfolio">Portfolio</a></li>
-        <li><a href="Testimonials">Testimonials</a></li>
-        <li><a href="contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Service</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className='footer__socials'>
         <a href="https://facebook.com" target='_blank' rel="noreferrer"><FaFacebookF /></a>
         <a href="https://instagram.com" target='_blank' rel="noreferrer"><FaInstagram /></a>
         <a href="https://twitter.com"  target='_blank' rel="noreferrer"><FaTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; chrisWO92. All rights reserved</small>
       </div>
 
     </footer>
