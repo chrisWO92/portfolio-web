35f3ba0d03f5c6d386e3642178506c61a207a908
chrisWO92 <chrisworkingonline@gmail.com>
2023-03-10 14:15:25 -0300

    fist personalization: adding video bg

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,14 @@ import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+import Video from '../../assets/header_video.mp4'
 
 const Header = () => {
   return (
     <header>
+      <div className="header-bg">
+        <video src={Video} autoplay='autoplay' muted loop type="video/mp4" id='video-bg'/>
+      </div>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>CrisWO92</h1>
