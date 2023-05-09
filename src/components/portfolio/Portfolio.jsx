54b51323d6d3001ceade2120270216c7ad761432
chrisWO92 <chrisworkingonline@gmail.com>
2023-05-08 21:02:43 -0300

    updating portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,43 +1,51 @@
 import React from 'react';
 import './portfolio.css';
-import IMG5 from '../../assets/attack-on-titan-game.png'
-import IMG1 from '../../assets/pomodoro-timer.png'
-import IMG2 from '../../assets/drum-machine.png'
-import IMG3 from '../../assets/online-calculator.png'
-import IMG4 from '../../assets/random-quote-machine.png'
+import IMG1 from '../../assets/todo-machine.png'
+import IMG2 from '../../assets/attack-on-titan-game.png'
+import IMG3 from '../../assets/pomodoro-timer.png'
+import IMG4 from '../../assets/drum-machine.png'
+import IMG5 from '../../assets/online-calculator.png'
+import IMG6 from '../../assets/random-quote-machine.png'
 
 const data = [
   {
-    id: 5,
-    image: IMG5,
-    title: 'Attack on Titan Game',
-    github: 'https://github.com/chrisWO92/attack-on-titan-game',
-    demo: 'https://jolly-mermaid-3cbc8f.netlify.app/'
-  },
-  {
     id: 1,
     image: IMG1,
+    title: 'Todo-Machine',
+    github: 'https://github.com/chrisWO92/todo-machine',
+    demo: 'https://chriswo92.github.io/todo-machine/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Attack on Titan Game',
+    github: 'https://github.com/chrisWO92/attack-on-titan-game',
+    demo: 'https://chriswo92.github.io/attack-on-titan-game/'
+  },
+  {
+    id: 3,
+    image: IMG3,
     title: '25 + 5 Pomodoro Timer',
     github: 'https://github.com/chrisWO92/pomodoro-timer',
     demo: 'https://fancy-wisp-2164a4.netlify.app'
   },
   {
-    id: 2,
-    image: IMG2,
+    id: 4,
+    image: IMG4,
     title: 'Drum Machine',
     github: 'https://github.com/chrisWO92/drum-machine',
     demo: 'https://warm-truffle-5bf2e3.netlify.app'
   },
   {
-    id: 3,
-    image: IMG3,
+    id: 5,
+    image: IMG5,
     title: 'Online Calculator',
     github: 'https://github.com/chrisWO92/online-calculator',
     demo: 'https://resilient-gnome-0c3caa.netlify.app'
   },
   {
-    id: 4,
-    image: IMG4,
+    id: 6,
+    image: IMG6,
     title: 'Random Quote Machine',
     github: 'https://github.com/chrisWO92/random-quote-machine',
     demo: 'https://eloquent-torrone-398a5d.netlify.app'
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import './testimonials.css';
 import AVATAR from '../../assets/avatar-client.png'
-
-// import { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
-//import "./styles.css";
-
-// import required modules
 import { Pagination } from "swiper";
 
 const testimonials = [
